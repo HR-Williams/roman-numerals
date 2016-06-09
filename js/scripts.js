56f3be5d7cb26a40d7dae6476a86b3b23153eb52
Halle Williams and Shradha Pulla <halle@>
2016-06-09 11:04:08 -0700

    Make if/else statements work

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,51 @@
-// user interface logic
 $(document).ready(function() {
-  $("form#converter").submit(function(event){
+  $("form#converter").submit(function(event) {
     event.preventDefault();
     var numberInput = $("input#number").val();
-    var result =
-    $("#result").show();
+
+    var romanSymbols = ["I", "V", "X", "L", "C", "D", "M"];
+    var romanValues = [1, 5, 10, 50, 100, 500, 1000];
+
+    if (numberInput <= "3") {
+
+
+      var result = numberInput + romanSymbols[0];
+      alert(result);
+    } else {
+      alert("Failed");
+    }
   });
 });
 
-// business logic
+
+
+
+
+// user interface logic
+// $(document).ready(function() {
+//   $("form#converter").submit(function(event) {
+//     event.preventDefault();
+//     var numberInput = $("input#number").val();
+//     // var numberFunction = "";
+//
+//     // $(".roman-numeral").text(result)
+//     // $("#result").show();
+//
+//
+// // business logic
+//
+//     if (numberInput === "1") {
+//       alert("Working!!");
+//     } else {
+//       alert("Failed");
+//     }
+//   });
+// });
+
+
+//
+//
+// var results = function(numberFunction){
+//   for (var i = 0; i < numberFunction.length; i++) {
+//
+//   }
