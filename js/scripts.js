ce612f1b43ed2505f26269cfc437f0c218ab2798
Halle Williams and Shradha Pulla <halle@>
2016-06-09 14:15:14 -0700

    Add conditional if/else statements to define numbers

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,33 @@ $(document).ready(function() {
     var romanSymbols = ["I", "V", "X", "L", "C", "D", "M"];
     var romanValues = [1, 5, 10, 50, 100, 500, 1000];
 
-    if (numberInput === "5") {
+    if (numberInput === "1") {
+      var romanOne = "I";
+      alert(romanOne);
+    } else if (numberInput === "2") {
+      var romanTwo = "II";
+      alert(romanTwo);
+    } else if (numberInput ==="3") {
+      var romanThree = "III";
+      alert(romanThree);
+    } else if (numberInput === "4") {
+      var romanFour = 
+      alert(romanFour);
+    } else if (numberInput === "5") {
       var romanFive = "V";
       alert(romanFive);
+    } else if (numberInput === "6") {
+      var romanSix = "VI";
+      alert(romanSix);
+    } else if (numberInput === "7") {
+      var romanSeven = "VII";
+      alert(romanSeven);
+    } else if (numberInput === "8") {
+      var romanEight = "VIII";
+      alert(romanEight);
+    // } else if (numberInput === "9") {
+    //   var romanNine =
+    //   alert(romanNine);
     } else if (numberInput === "10") {
       var romanTen = "X";
       alert(romanTen);
@@ -24,22 +48,8 @@ $(document).ready(function() {
     } else if (numberInput === "1000") {
       var romanThousand = "M";
       alert(romanThousand);
-    } else if (numberInput != "5" || numberInput != "10" || numberInput != "50" || numberInput != "100" || numberInput != "500" || numberInput != "1000") {
-      for (var i = 1; i <= numberInput.length; i ++) {
-        if (numberInput[i] <= "3") {
-
-        } else if (numberInput[i] === "4") {
-
-        } else if (numberInput[i] <= "8") {
-
-        } else if (numberInput[i] === "9") {
-
-        } else {
-          alert("Error");
-        }
-      };
     } else {
-      alert("Error Error");
+      alert("Error");
     }
   });
 });
